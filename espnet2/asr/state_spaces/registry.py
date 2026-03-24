449b6b898b2557674ba5d49c0b3e748eefaa22d8
r0883470 <pepijn.haenen@student.kuleuven.be>
2026-03-24 14:18:41 +0100

    gitignore adjusted + asr_s4 model

--- espnet2/asr/state_spaces/registry.py
+++ espnet2/asr/state_spaces/registry.py
@@ -1,5 +1,8 @@
 layer = {
     "s4": "espnet2.asr.state_spaces.s4.S4",
+    "s6": "espnet2.asr.state_spaces.s6.Mamba1",  # Mamba1 as s6
+    "mamba1": "espnet2.asr.state_spaces.s6.Mamba1",
+    "mamba2": "espnet2.asr.state_spaces.s6.Mamba2",
     "ff": "espnet2.asr.state_spaces.ff.FF",
     "mha": "espnet2.asr.state_spaces.attention.MultiHeadedAttention",
 }
